d7f26ebeb193fef936e062c95b8af1eafa42a301
Juozas Auskalnis <jauskaln@redhat.com>
2019-07-20 14:12:46 +0100

    added auto csr approve

--- upi-ocp-install.sh
+++ upi-ocp-install.sh
@@ -1,4 +1,4 @@
-! /bin/bash
+#! /bin/bash
 
 echo "CONTROL PLANE PROVISIONING STARTED..."
 terraform apply -auto-approve
@@ -24,11 +24,18 @@ echo "******************************************"
 
 sleep 5
 
-echo "WORKERS PROVISIONING STARTED..."
+echo "WORKERS PROVISIONING STARTED...\n"
 
 cd worker
 
 terraform apply -auto-approve
+
+worker_count=`cat terraform.tfvars | grep worker_count | awk '{print $3}'`
+
+while [ $(oc get csr | grep worker | grep Approved | wc -l) != $worker_count ]; do
+	oc get csr -o json | jq -r '.items[] | select(.status == {} ) | .metadata.name' | xargs oc adm certificate approve
+	sleep 3
+done
 
 cd ../
 
