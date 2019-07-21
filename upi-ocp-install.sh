9132601f5e63219799f26ec14d3d28b70a750ca6
Juozas Auskalnis <j.auskalnis@gmail.com>
2019-07-21 19:43:36 +0100

    Update upi-ocp-install.sh

--- upi-ocp-install.sh
+++ upi-ocp-install.sh
@@ -82,7 +82,7 @@ cd ../
 
 echo "Searching for any Pending CSRs"
 
-worker_count=`cat terraform.tfvars | grep worker_count | awk '{print $3}'`
+worker_count=`cat worker/terraform.tfvars | grep worker_count | awk '{print $3}'`
 
 while [ $(oc get csr | grep worker | grep Approved | wc -l) != $worker_count ]; do
 	oc get csr -o json | jq -r '.items[] | select(.status == {} ) | .metadata.name' | xargs oc adm certificate approve
